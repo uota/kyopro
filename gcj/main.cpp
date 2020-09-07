49a88c7da64cba35897ef85fc15a78a03061fda5
“uota” <ennpitsu@gmail.com>
2020-09-07 23:21:17 +0900

    gcjテンプレートの追加

---
+++ gcj/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pii;
+typedef tuple<ll, ll, ll> ti;
+ 
+#define REP(a,b,c) for(ll a=(b);a<(c);a++)
+#define PER(a,b,c) for(ll a=(b);a>=(c);a--)
+
+ll ii(){ ll x; cin >> x; return x; }
+string is(){ string x; cin >> x; return x; }
+void oi(ll x){ cout << x; }
+void od(ld x){ cout << fixed << setprecision(10) << x; }
+void os(string x){ cout << x; }
+void oie(ll x){ cout << x << endl; }
+void ode(ld x){ cout << fixed << setprecision(10) << x << endl; }
+void ose(string x){ cout << x << endl; }
+void oe(){ cout << endl; }
+
+void out(ll id){
+    cout << "Case #" << id+1 << ": " << endl;
+}
+
+int main(){
+
+    return 0;
+}
